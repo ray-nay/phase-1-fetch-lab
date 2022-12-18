5e1bfa59aa92bf7654e3dafe547482160813fe08
Renee Mumbi <1723renee@gmail.com>
2022-12-18 15:54:26 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then((res)=> res.json())
+  .then(json => renderBooks(json))
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+    const h3 = document.createElement('h3');
+    h3.textContent = book.name;
+    main.appendChild(h3);
   });
 }
 
